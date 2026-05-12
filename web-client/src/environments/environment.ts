d5ea00555b2a30f54292076c6cb02e8db499271f
Rocio <75289667+rociocc@users.noreply.github.com>
2026-05-12 16:08:53 -0500

    add environment config file

---
+++ web-client/src/environments/environment.ts
@@ -0,0 +1,8 @@
+import { AppEnvironment } from "./environment.model";
+
+export const environment: AppEnvironment = {
+    production: true,
+    productServiceUrl: '',
+    stockServiceUrl: '',
+    paymentServiceUrl: ''
+};
